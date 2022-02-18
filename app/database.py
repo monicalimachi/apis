ecda3fd9a565d8295b45c59e0568c4d9b48c3b81
Monica Limachi <dolce.monikita@gmail.com>
2022-02-17 22:03:22 -0400

    updated config envs and settings

--- app/database.py
+++ app/database.py
@@ -1,12 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+from .config import settings
 
 #SQLALCHEMY_DATABASE_URL = "postgresql://<username>:<password>@<ip=address/hostname>/<database_name>"
-SQLALCHEMY_DATABASE_URL = "postgresql://dulcinea:root@localhost:15432/fastapi"
+#SQLALCHEMY_DATABASE_URL = "postgresql://dulcinea:root@localhost:15432/fastapi"
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
@@ -19,3 +23,19 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+#Connection to DB postgresql developer server For reference run sql without using sqlAlchemy
+""" while True:
+
+    try:
+        conn = psycopg2.connect(host='localhost', database='fastapi', port=15432, user = 'dulcinea', password='root', cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Database connection was succesfull!")
+        break
+    except Exception as error:
+        print ("Connecting to database failed")
+        print("Error: ", error)
+        time.sleep(2)
+ """
+
--- app/routers/post.py
+++ app/routers/post.py
@@ -1,7 +1,7 @@
 from fastapi import Depends, FastAPI, Response, status, HTTPException, APIRouter
 from .. import models, schemas, oauth2
 from sqlalchemy.orm import Session
-from typing import List
+from typing import List, Optional
 from ..database import get_db
 
 router=APIRouter(
@@ -10,12 +10,13 @@ router=APIRouter(
     ) 
 
 @router.get("/", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db), current_user: int =Depends(oauth2.get_current_user)):
+def get_posts(db: Session = Depends(get_db), current_user: int =Depends(oauth2.get_current_user), limit: int = 10, skip:int=0, search:Optional[str]=""):
     #cursor.execute("""SELECT * FROM posts """)
     #posts = cursor.fetchall()
     #print(posts)
     #posts = db.query(models.Post).filter(models.Post.owner_id==current_user.id).all()
-    posts = db.query(models.Post).all()
+    print(limit)
+    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
 
     return posts
 
